c6fdc6add3410d31b7b45c4b4491dd7b2e596490
Tobias <154422539+Tobyy85@users.noreply.github.com>
2026-05-12 20:49:40 +0200

    refactor(routes): specify RouteObject type for routes definition

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,7 +1,9 @@
+import type { RouteObject } from 'react-router-dom'
+
 import ErrorPage from '@/pages/ErrorPage'
 import HomePage from '@/pages/HomePage'
 
-export const routes = [
+export const routes: RouteObject[] = [
     {
         path: '/',
         element: <HomePage />,
